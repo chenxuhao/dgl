25b8db6f427755ed5d42f11d1dc082ed00e3a8e6
chenxuhao <xhchen@br006.pvt.bridges.psc.edu>
2020-03-15 18:34:37 -0400

    gcn command

--- run-gcn-cora.sh
+++ run-gcn-cora.sh
@@ -1,1 +1,2 @@
-python examples/pytorch/gcn/train.py --dataset cora --n-epochs 10 --nt 56
+echo "python examples/pytorch/gcn/train.py --dataset cora --n-epochs 10 --gpu 0"
+python examples/pytorch/gcn/train.py --dataset cora --n-epochs 10 --gpu 0
